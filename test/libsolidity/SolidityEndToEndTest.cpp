5b1926a822fef3029c5e2c6bb887f69eb20d9ea1
chriseth <chris@ethereum.org>
2020-10-08 23:05:03 +0200

    fixup! Update existing tests.

--- test/libsolidity/SolidityEndToEndTest.cpp
+++ test/libsolidity/SolidityEndToEndTest.cpp
@@ -1778,10 +1778,11 @@ BOOST_AUTO_TEST_CASE(blockhash)
 			function g() public returns (bool) { counter++; return true; }
 			function f() public returns (bytes32[] memory r) {
 				r = new bytes32[](259);
-				for (uint i = 0; i < 259; i++)
+				for (uint i = 0; i < 259; i++) {
 					unchecked {
 						r[i] = blockhash(block.number - 257 + i);
 					}
+				}
 			}
 		}
 	)";
